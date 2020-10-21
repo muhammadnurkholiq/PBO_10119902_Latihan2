a39f0d657762e69b23f844ef426bfe7e99319d34
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:31:41 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan2/src/main/java/com/mycompany/pbo_10119902_latihan2/PBO_10119902_Latihan2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan2;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan2 {
+    public static void main(String[] args) {
+        //Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika =  false;
+        nilaiKarakter = 'D';
+        
+        System.out.println();
+        System.out.println("Isi Variabel Nilai = " + nilaiInt);
+        System.out.println("Isi Variabel PHI = " + PHI);
+        System.out.println("Isi Variabel Logika = " + nilaiLogika);
+        System.out.println("Isi Variabel Karakter = " + nilaiKarakter);    
+    }
+}
